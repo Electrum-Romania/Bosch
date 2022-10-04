cb5da5d2a84f1714a3f4d8a4c180c99b5549e1a6
snouragan <ahaiducescu@yahoo.com>
2022-10-04 16:46:39 +0300

    streamer not working

--- Brain/src/utils/camerastreamer/LaneRecognitionThread.py
+++ Brain/src/utils/camerastreamer/LaneRecognitionThread.py
@@ -2,11 +2,14 @@ from src.templates.threadwithstop import ThreadWithStop
 
 class LaneRecognitionThread(ThreadWithStop):
 
-    def __init__(self, outPs):
+    def __init__(self, inPs, outPs):
         super(LaneRecognitionThread, self).__init__()
         self.daemon = True
 
         self.outPs = outPs
+        self.inPs = inPs
 
     def run(self):
-        print('a');
+        while True:
+            for inP in self.inPs:
+                print(inP.recv())
