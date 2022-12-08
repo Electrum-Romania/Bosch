86d8894ada17ee13af0cc63ed5a8f5b5104c879d
snouragan <ahaiducescu@yahoo.com>
2022-12-08 17:49:56 +0200

    display static images

--- Brain/src/utils/camerastreamer/CameraReceiverProcess.py
+++ Brain/src/utils/camerastreamer/CameraReceiverProcess.py
@@ -109,12 +109,6 @@ class CameraReceiverProcess(WorkerProcess):
                 image = np.reshape(image, self.imgSize)
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-                # lane
-                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                blur = cv2.GaussianBlur(gray, (5, 5), 0)
-                edges = cv2.Canny(image, 100, 200)
-                
-
                 # ----------------------- show images -------------------
                 cv2.imshow('Image', image) 
                 cv2.waitKey(1)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-
-    return render(request, 'index.html')
+    template = loader.get_template('index.html')
+    context = {
+        'image': 'rat-car-9553952.png',
+    }
+    return HttpResponse(template.render(context, request))
