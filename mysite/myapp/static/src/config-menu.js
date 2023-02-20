a1dfb8330a64045ddd1e2d7c8ec61799965ef891
snouragan <ahaiducescu@yahoo.com>
2023-02-20 17:56:34 +0200

    added gitignore + analysis post

--- mysite/myapp/static/src/config-menu.js
+++ mysite/myapp/static/src/config-menu.js
@@ -5,3 +5,30 @@ $('.menu-button').on('click', function(e) {
     $('#' + this.id + "-icon").removeClass("visible").siblings().addClass('visible');
     $('#' + this.id + "-div").addClass("visible").siblings().removeClass('visible');
 });
+
+var main = ""
+
+$('.horizontal.menu-button').on('click', function(e) {
+    e.preventDefault();
+    var $el = $(this);
+    main = this.id;
+});
+
+var second = ""
+
+$('.vertical.menu-button').on('click', function(e) {
+    e.preventDefault();
+    var $el = $(this);
+    second = this.id;
+});
+
+$('input[type=range]').on('input', function() {
+    
+    $.ajax({
+        url: "load",
+        type: "POST",
+        data: {
+            'analysis': main + " " + second,
+        },
+    })  
+});
