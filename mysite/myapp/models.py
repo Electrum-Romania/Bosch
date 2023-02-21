5fd08906387342c689dc5d7d2732927d712b2c21
snouragan <ahaiducescu@yahoo.com>
2023-02-21 18:55:27 +0200

    analysis mode

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class EdgeDetection(models.Model):
     name = models.CharField(default="edges", max_length=20)
 
-    lane_perspective_floor = models.PositiveIntegerField(default=250)
-    lane_perspective_roof = models.PositiveIntegerField(default=750)
-    lane_perspective_startfloor = models.PositiveIntegerField(default=250)
-    lane_perspective_stopfloor = models.PositiveIntegerField(default=750)
-    lane_perspective_startroof = models.PositiveIntegerField(default=250)
-    lane_perspective_stoproof = models.PositiveIntegerField(default=750)
+    lane_perspective_floor = models.PositiveIntegerField(default=0)
+    lane_perspective_roof = models.PositiveIntegerField(default=0)
+    lane_perspective_startfloor = models.PositiveIntegerField(default=0)
+    lane_perspective_stopfloor = models.PositiveIntegerField(default=0)
+    lane_perspective_startroof = models.PositiveIntegerField(default=0)
+    lane_perspective_stoproof = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
